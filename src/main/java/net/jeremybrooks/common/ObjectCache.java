4e456a10c514d9df229e4fe02795f9b38c90c925
jeremybrooks <jeremyb@whirljack.net>
2013-10-22 16:15:22 -0400

    Added copyright notices. Cleaned up some code.

--- src/main/java/net/jeremybrooks/common/ObjectCache.java
+++ src/main/java/net/jeremybrooks/common/ObjectCache.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2013, Jeremy Brooks
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package net.jeremybrooks.common;
 
 import net.jeremybrooks.common.util.IOUtil;
@@ -146,7 +164,7 @@ public class ObjectCache {
 		}
 
 		Object obj = null;
-		ObjectInputStream in = null;
+		ObjectInputStream in;
 		File cacheFile = new File(this.cacheDir, name);
 
 		if (cacheFile.exists()) {
@@ -171,19 +189,26 @@ public class ObjectCache {
 	 * If the object does not exist in the cache, nothing happens.
 	 *
 	 * @param name identifies the object to delete.
+	 * @return number of delete failures.
 	 */
-	public void delete(String name) {
+	public int delete(String name) {
 		if (StringUtil.isNullOrEmpty(name)) {
-			return;
+			return 0;
 		}
+		int failures = 0;
 		File cacheFile = new File(this.cacheDir, name);
 
 		if (cacheFile.exists()) {
-			cacheFile.delete();
-			if (logger.isDebugEnabled()) {
-				logger.debug("Deleted " + cacheFile.getAbsolutePath());
+			if (cacheFile.delete()) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("Deleted " + cacheFile.getAbsolutePath());
+				}
+			} else {
+				failures++;
+				logger.warn("Failed to delete file " + cacheFile.getAbsolutePath());
 			}
 		}
+		return failures;
 	}
 
 
@@ -191,12 +216,18 @@ public class ObjectCache {
 	 * Clear the cache directory.
 	 * <p/>
 	 * This will delete all the objects in the cache.
+	 *
+	 * @return number of file delete failures.
 	 */
-	public void clear() {
+	public int clear() {
+		int failures = 0;
 		File[] files = this.cacheDir.listFiles();
 		for (File f : files) {
-			f.delete();
+			if (!f.delete()) {
+				failures++;
+			}
 		}
+		return failures;
 	}
 
 }
